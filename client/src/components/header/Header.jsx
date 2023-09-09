3b22c5d4079d8c94204d4ab5469bc7d14197f1dc
Chinmay Desai <58367566+ChinmayDesai2005@users.noreply.github.com>
2023-09-10 02:45:43 +0530

    header added (basic)

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import './header.css';
+import React from "react";
+import "./header.css";
 
 function Header() {
   return (
-    <section className='header'>Hello World</section>
-  )
+    <section className="header">
+      <nav>
+        <div className="nav-logo">TaxSarthi</div>
+        <div className="nav-contents">
+          <ul>
+            <li>
+              <a href="/">Pricing +</a>
+              <ul>
+                <li>
+                  <a href="#">Something</a>
+                </li>
+                <li>
+                  <a href="#">Something</a>
+                </li>
+                <li>
+                  <a href="#">Something</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/">About +</a>
+              <ul>
+                <li>
+                  <a href="#">Something</a>
+                </li>
+                <li>
+                  <a href="#">Something</a>
+                </li>
+                <li>
+                  <a href="#">Something</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/">Contact +</a>
+              <ul>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+                <li>
+                  <a href="#">Socials</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </section>
+  );
 }
 
-export default Header
+export default Header;
