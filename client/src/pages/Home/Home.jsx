b84898a32df3792808c7f2ccebc321ee62909ae1
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-09-22 16:13:38 +0530

    fixed header and home page
    Signed-off-by: Gautam-666 <2022.gautam.rai@ves.ac.in>

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
-import Carousel from "react-bootstrap/Carousel";
+import Carousel from "../../components/mis/Carousel/Carousel";
+import Marquee from 'react-fast-marquee'
+import FaqSec from "../../components/mis/FaqSec";
 
 function Home() {
   const navigate = useNavigate();
   return (
-    <div>
-      <section id="home">
+    <>
+      <section id="home" style={{ margin: "0" }}>
         <div className="main">
           <div className="front">
             <hi className="front-title">TaxSarthi</hi>
@@ -23,74 +25,47 @@ function Home() {
           </div>
         </div>
       </section>
-      <section id="home-tutorial">
-        <p class="home-tutorial-text">
-          Watch this short tutorial to learn how to use TaxSarthi
-        </p>
-        <iframe
-          class="home-tutorial-embed"
-          src="https://www.youtube.com/embed/GRq_tC7qbaE?si=t22-L-jLYQn4QUdm"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </section>
-      <section id="home-stats">
-        <div class="stats-title">TaxSarthi's Statistics</div>
-        <div className="stats-group">
-          <div class="stats-heading">Clients Satisfied: </div>
-          <div class="stats-amount">200 +</div>
-        </div>
-        <div className="stats-group">
-          <div class="stats-heading">Tax Savings Acheived: </div>
-          <div class="stats-amount">$500 +</div>
-        </div>
-        <div className="stats-group">
-          <div class="stats-heading">Success Rate: </div>
-          <div class="stats-amount">100%</div>
-        </div>
-      </section>
-      <section id="carousel">
-        <Carousel>
-          <Carousel.Item>
-            {/* <ExampleCarouselImage text="First slide" /> */}
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1694618237208-728651429710?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            {/* <ExampleCarouselImage text="Second slide" /> */}
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1682685794761-c8e7b2347702?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-            />
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            {/* <ExampleCarouselImage text="Third slide" /> */}
-            <img
-              className="d-block w-100"
-              src="https://images.unsplash.com/photo-1682685797088-283404e24b9d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-            />
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </section>
-    </div>
+      <div className="home-div">
+        <section id="home-tutorial">
+          <p class="home-tutorial-text">
+            Watch this short tutorial to learn how to use TaxSarthi
+          </p>
+          <iframe
+            class="home-tutorial-embed"
+            src="https://www.youtube.com/embed/GRq_tC7qbaE?si=t22-L-jLYQn4QUdm"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </section>
+        <section id="home-stats">
+          <div class="stats-title">Loved by over 1 million tax payers</div>
+          <div className="main-stats">
+          <div className="stats-group">
+            <div class="stats-amount">₹20 Cr+</div>
+            <div class="stats-heading">assets managed</div>
+          </div>
+          <div className="stats-group">
+            <div class="stats-amount">₹50 Cr+</div>
+            <div class="stats-heading">worth taxes filed</div>
+          </div>
+          <div className="stats-group">
+            <div class="stats-amount">₹30 Cr+</div>
+            <div class="stats-heading">worth taxes saved</div>
+          </div>
+          </div>
+        </section>
+        <section id="carousel">
+        <Marquee speed={25} pauseOnClick={true} gradient gradientWidth={50} delay={30}>
+          <Carousel />
+        </Marquee>
+        </section>
+        <section className="faq">
+          <FaqSec />
+        </section>
+      </div>
+    </>
   );
 }
 
