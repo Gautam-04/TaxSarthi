54f104fc5901ed22560f067027d606549bb5bc1d
Chinmay Desai <chinmaydesai2005@gmail.com>
2023-12-02 22:15:13 +0530

    Blogs Added

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,21 @@ import Header from "./components/header/Header";
 import ErrorPage from "./pages/Error/ErrorPage";
 import Login from "./pages/Auth/Login";
 import Home from "./pages/Home/Home";
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AboutTaxes from "./pages/Taxes/About Taxes/AboutTaxes";
 import Docs from "./pages/Docs-list/Docs";
 import FormFilling from "./pages/Main Form/Form";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import WhatAreTaxes from "./pages/Taxes/About Taxes/WhatAreTaxes/what-are-taxes";
+import TypesOfTaxes from "./pages/Taxes/About Taxes/TypesOfTaxes/types-of-taxes";
+import ItrFiling from "./pages/Taxes/About Taxes/ItrFiling/itr-filing";
+import TaxPlanning from "./pages/Taxes/About Taxes/TaxPlanning/tax-planning";
+import SaveTaxes from "./pages/Taxes/About Taxes/SaveTaxes/save-taxes";
+import TaxNotice from "./pages/Taxes/About Taxes/IncomeTaxNotice/tax-notice";
+import FAQs from "./pages/Taxes/About Taxes/FAQs/faqs";
 
 function App() {
-  
-
   return (
     <>
       <Router>
@@ -25,6 +26,14 @@ function App() {
           <Route path="/" element={<Home />} />
           {/* Taxes Routes */}
           <Route path="/taxes/about-taxes" element={<AboutTaxes />} />
+          {/* What are Taxes?*/}
+          <Route path="/taxes/what-are-taxes" element={<WhatAreTaxes />} />
+          <Route path="/taxes/types-of-taxes" element={<TypesOfTaxes />} />
+          <Route path="/taxes/itr-filing" element={<ItrFiling />} />
+          <Route path="/taxes/tax-planning" element={<TaxPlanning />} />
+          <Route path="/taxes/save-taxes" element={<SaveTaxes />} />
+          <Route path="/taxes/tax-notice" element={<TaxNotice />} />
+          <Route path="/taxes/faqs" element={<FAQs />} />
           <Route path="/taxes/tax-basics" element={<Home />} />
           <Route path="/taxes/other-taxes" element={<Home />} />
           {/* Savings Routes */}
