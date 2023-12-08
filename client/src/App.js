d815ad940a5087b27c5ff5f0cc63a5edc7e2f8e4
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-12-08 14:50:20 +0530

    Made a dynamic pdf system
    Signed-off-by: Gautam-666 <2022.gautam.rai@ves.ac.in>

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/header/Header";
 import ErrorPage from "./pages/Error/ErrorPage";
 import Login from "./pages/Auth/Login";
 import Home from "./pages/Home/Home";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
 import AboutTaxes from "./pages/Taxes/About Taxes/AboutTaxes";
 import Docs from "./pages/Docs-list/Docs";
 import FormFilling from "./pages/Main Form/Form";
@@ -16,45 +16,52 @@ import TaxPlanning from "./pages/Taxes/About Taxes/TaxPlanning/tax-planning";
 import SaveTaxes from "./pages/Taxes/About Taxes/SaveTaxes/save-taxes";
 import TaxNotice from "./pages/Taxes/About Taxes/IncomeTaxNotice/tax-notice";
 import FAQs from "./pages/Taxes/About Taxes/FAQs/faqs";
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
+import OldDoc from "./pages/Pdf Docs/Old Regin/OldDoc";
+import Blank from "./pages/Pdf Docs/Blank";
 
 function App() {
+  const location = useLocation();
+  const isBlankDocRoute = location.pathname === "/blankdoc";
+
   return (
     <>
-      <Header />
-        <Routes>
+      {!isBlankDocRoute && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Taxes Routes */}
+        <Route path="/taxes/about-taxes" element={<AboutTaxes />} />
+        {/* What are Taxes?*/}
+        <Route path="/taxes/what-are-taxes" element={<WhatAreTaxes />} />
+        <Route path="/taxes/types-of-taxes" element={<TypesOfTaxes />} />
+        <Route path="/taxes/itr-filing" element={<ItrFiling />} />
+        <Route path="/taxes/tax-planning" element={<TaxPlanning />} />
+        <Route path="/taxes/save-taxes" element={<SaveTaxes />} />
+        <Route path="/taxes/tax-notice" element={<TaxNotice />} />
+        <Route path="/taxes/faqs" element={<FAQs />} />
+        <Route path="/taxes/tax-basics" element={<Home />} />
+        <Route path="/taxes/other-taxes" element={<Home />} />
+        {/* Savings Routes */}
+        <Route path="/savings/smart-savings" element={<Home />} />
+        <Route path="/savings/tax-saving-investment" element={<Home />} />
+        <Route path="/savings/deductions-benefits" element={<Home />} />
+        {/* Filing Routes */}
+        <Route path="/filing/filing-your-taxes" element={<Home />} />
+        <Route path="/filing/organizing-document" element={<Home />} />
+        {/* Help Routes */}
+        <Route path="/help/reponding-to-notice" element={<Home />} />
+        <Route path="/help/disputes-resolution" element={<Home />} />
+        {/* Login Route */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/docs-list" element={<Docs />} />
+        <Route path="/form-filling" element={<FormFilling />} />
+        {/* Error Route */}
+        <Route path="/*" element={<ErrorPage />} />
 
-          <Route path="/" element={<Home />} />
-          {/* Taxes Routes */}
-          <Route path="/taxes/about-taxes" element={<AboutTaxes />} />
-          {/* What are Taxes?*/}
-          <Route path="/taxes/what-are-taxes" element={<WhatAreTaxes />} />
-          <Route path="/taxes/types-of-taxes" element={<TypesOfTaxes />} />
-          <Route path="/taxes/itr-filing" element={<ItrFiling />} />
-          <Route path="/taxes/tax-planning" element={<TaxPlanning />} />
-          <Route path="/taxes/save-taxes" element={<SaveTaxes />} />
-          <Route path="/taxes/tax-notice" element={<TaxNotice />} />
-          <Route path="/taxes/faqs" element={<FAQs />} />
-          <Route path="/taxes/tax-basics" element={<Home />} />
-          <Route path="/taxes/other-taxes" element={<Home />} />
-          {/* Savings Routes */}
-          <Route path="/savings/smart-savings" element={<Home />} />
-          <Route path="/savings/tax-saving-investment" element={<Home />} />
-          <Route path="/savings/deductions-benefits" element={<Home />} />
-          {/* Filing Routes */}
-          <Route path="/filing/filing-your-taxes" element={<Home />} />
-          <Route path="/filing/organizing-document" element={<Home />} />
-          {/* Help Routes */}
-          <Route path="/help/reponding-to-notice" element={<Home />} />
-          <Route path="/help/disputes-resolution" element={<Home />} />
-          {/* Login Route */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/docs-list" element={<Docs />} />
-          <Route path="/form-filling" element={<FormFilling />} />
-          {/* Error Route */}
-          <Route path="/*" element={<ErrorPage />} />
-
-        </Routes>
-        <Footer />
+        {/* Pdf docs Path */}
+        <Route path="/blankdoc" element={<Blank />} />
+      </Routes>
+      {!isBlankDocRoute && <Footer />}
       <ToastContainer
         position="bottom-center"
         autoClose={5000}
