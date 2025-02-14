5e6220ac48b1bcc8859c8378a3a6392cffb7742d
Gautam-04 <2022.gautam.rai@ves.ac.in>
2025-02-14 12:55:40 +0530

    fixed ui issues
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

--- client/src/components/mis/Card/Card.jsx
+++ client/src/components/mis/Card/Card.jsx
@@ -4,15 +4,10 @@ import { useNavigate } from "react-router-dom";
 import "./Card.css";
 
 function truncateText(text, maxLength) {
-  if (text.length <= maxLength) {
-    return text;
-  } else {
-    return text.substring(0, maxLength) + "...";
-  }
+  return text.length > maxLength ? text.substring(0, maxLength) + "..." : text;
 }
 
-function BlogCard(props) {
-  const { title, body, useNavigateTo } = props;
+function BlogCard({ title, body, useNavigateTo }) {
   const navigate = useNavigate();
   const truncatedBody = truncateText(body, 100);
 
@@ -20,42 +15,16 @@ function BlogCard(props) {
     if (useNavigateTo) {
       navigate(useNavigateTo);
     } else {
-      // Handle button click logic without navigation
       console.log("Button clicked without navigation");
     }
   };
 
   return (
-    <Card
-      // style={{
-      //   width: "18rem",
-      //   boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-      //   borderRadius: "20px",
-      //   height: "100%",
-      //   border: "2px solid #b3d89c",
-      // }}
-      className="card"
-    >
+    <Card className="card">
       <Card.Body>
-        <Card.Title style={{ fontSize: "2rem" }}>{title}</Card.Title>
-        <Card.Text style={{ color: "var(--blue-dark)", fontSize: "1.12rem" }}>
-          {truncatedBody}
-        </Card.Text>
-        <button
-          onClick={handleButtonClick}
-          // style={{
-          //   padding: "0 30px",
-          //   width: "auto",
-          //   height: "50px",
-          //   borderRadius: "30px",
-          //   fontWeight: "600",
-          //   fontSize: "1.15rem",
-          //   backgroundColor: "#b3d89c",
-          //   color: "black",
-          //   transition: "background-color 0.2s ease-in-out",
-          // }}
-          className="card-button"
-        >
+        <Card.Title className="card-title">{title}</Card.Title>
+        <Card.Text className="card-text">{truncatedBody}</Card.Text>
+        <button onClick={handleButtonClick} className="card-button">
           Read More
         </button>
       </Card.Body>
